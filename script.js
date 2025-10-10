f05d16b5f3d4eabd28d200395e4e0665dc0ec44b
blahblahblah99 <136532476+blahblahblah99@users.noreply.github.com>
2025-10-10 15:17:34 +1100

    comments

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// create the myMovie object and log it out
 let myMovie = {title:"The longest day", year:1964};
 console.log(myMovie);
 
+// add a rating and summary property to the object and log it out
 myMovie.rating = 5;
 myMovie.summary = "World War II movie about the Normandy landings";
 console.log(myMovie);
 
+// change the rating and year properties and log it out
 myMovie.rating = 4;
 myMovie.year = 1962;
 console.log(myMovie);
 
+// delete the summary property from the object and log it out
 delete myMovie.summary;
 console.log(myMovie);
