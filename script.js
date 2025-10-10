625d06294aa091ed91414f4b108c10b00161a703
blahblahblah99 <136532476+blahblahblah99@users.noreply.github.com>
2025-10-10 14:55:51 +1100

    added new properties

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let myMovie = {title:"The longest day", year:1964};
 console.log(myMovie);
+
+myMovie.rating = 5;
+myMovie.summary = "World War II movie about the Normandy landings";
+console.log(myMovie);
